f4966f70089d7c3929680dd405063993be4a9841
codecrook <hello@codecrook.io>
2018-08-14 17:27:28 +0530

    Added styles and animation What's Done: - Added styles to the page. - Added transition effect to the elements. - Key animates when pressed.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
-window.addEventListener('keydown', playSound);
-const keys = Array.from(document.querySelectorAll('.key'));
-
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+(function() {
+    const keys = Array.from(document.querySelectorAll('.key'));
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
     
-    if (!audio) return;
+    window.addEventListener('keydown', playSound);
     
-    audio.currentTime = 0;
-    audio.play();
+    /**
+     *Plays sound when key is pressed
+     *Adds class to the element
+    **/
+    function playSound(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+        if (!audio) return;
+        
+        key.classList.add('playing');
+        
+        audio.currentTime = 0;
+        audio.play();
+  }
     
-}
+    /**
+     *Removes the 'playing' class from the element when transition is completed
+    **/
+    function removeTransition(e) {
+        if (e.propertyName !== 'transform') return;
+        e.target.classList.remove('playing');
+    }
+})();
